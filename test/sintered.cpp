6fbece6b220e63d8c1c77421095fc1ed1e7aab60
Egor Demidov <ed242@njit.edu>
2024-01-26 22:13:41 -0500

    Improved the sinter bridge model and added a unit test

--- test/sintered.cpp
+++ test/sintered.cpp
@@ -12,7 +12,9 @@
 #include <libgran/contact_force/contact_force.h>
 #include <libgran/granular_system/granular_system.h>
 #include <libgran/sinter_bridge/sinter_bridge.h>
+#include <libgran/sinter_bridge/alt_sinter_bridge.h>
 
+#include "mass_distribution.h"
 #include "../writer.h"
 
 std::mt19937_64 mt(0);
@@ -32,12 +34,12 @@ Eigen::Vector3d generate_random_unit_vector() {
 // field_container_t and field_value_t
 // Therefore, we need to create an alias where real_t is specialized
 template <typename field_container_t, typename field_value_t>
-using sinter_step_handler_double = sinter_step_handler<field_container_t, field_value_t, double>;
+using alt_sinter_step_handler_double = alt_sinter_step_handler<field_container_t, field_value_t, double>;
 
 int main() {
     // General simulation parameters
     const double dt = 1e-13;
-    const double t_tot = 3.0e-7;
+    const double t_tot = 1.0e-7;
     const auto n_steps = size_t(t_tot / dt);
     const size_t n_dumps = 300;
     const size_t dump_period = n_steps / n_dumps;
@@ -60,11 +62,10 @@ int main() {
 
     // Initialize the particles
     std::vector<Eigen::Vector3d> x0, v0, theta0, omega0;
-    // 15 particles in the x-y plane
-    for (size_t i = 0; i < 6; i ++) {
-        auto y = -5.0 * r_part + double(i) * 2.0 * r_part;
-        for (size_t j = 0; j < 6; j ++) {
-            auto x = -5.0 * r_part + double(j) * 2.0 * r_part;
+    for (size_t i = 0; i < 3; i ++) {
+        auto y = -1.0 * r_part + double(i) * 2.0 * r_part;
+        for (size_t j = 0; j < 3; j ++) {
+            auto x = -1.0 * r_part + double(j) * 2.0 * r_part;
             x0.emplace_back(x, y, 0.0);
             x0.emplace_back(x, y, -2.0*r_part);
             x0.emplace_back(x, y, -4.0*r_part);
@@ -74,7 +75,7 @@ int main() {
             Eigen::Vector3d init_vel;
             if (j == 0)
                 init_vel = {0.0, 0.0, 5.0};
-            else if (j == 5)
+            else if (j == 2)
                 init_vel = {0.0, 5.0, 0.0};
             else
                 init_vel = Eigen::Vector3d::Zero();
@@ -89,18 +90,8 @@ int main() {
     std::fill(theta0.begin(), theta0.end(), Eigen::Vector3d::Zero());
     std::fill(omega0.begin(), omega0.end(), Eigen::Vector3d::Zero());
 
-    std::cout << "Number of particles: " << x0.size() << std::endl;
-
-    // Create an instance of contact force model
-    // Using field type Eigen::Vector3d with real type double
-    contact_force_functor<Eigen::Vector3d, double> contact_force_model(x0.size(),
-       k, gamma_n, k, gamma_t, mu, phi, k, gamma_r, mu_o, phi, k, gamma_o, mu_o, phi,
-       r_part, mass, inertia, dt, Eigen::Vector3d::Zero(), 0.0);
-
-    // Create an instance of sintered model
-    sinter_functor<Eigen::Vector3d, double> sinter_model(x0.size(), k,
-         gamma_n, r_part, mass, inertia, Eigen::Vector3d::Zero(), 0.0, 1.0e-9,
-         x0.begin(), x0.end(), contact_force_model.enabled_contacts, generate_random_unit_vector);
+    alt_sinter_functor<Eigen::Vector3d, double> sinter_model(x0.size(), k, gamma_n,  k, gamma_n, k, gamma_t, mu, phi, k, gamma_r, mu_o, phi, k, gamma_o, mu_o, phi,
+       r_part, mass, inertia, dt, Eigen::Vector3d::Zero(), 0.0, x0.begin(), generate_random_unit_vector, 1.0e-9);
 
     // We need to use a custom step handler with the sintering model
     // Get the custom step handler instance from the sinter model
@@ -110,23 +101,34 @@ int main() {
     // Using velocity Verlet integrator for rotational systems and a default
     // step handler for rotational systems
     granular_system<Eigen::Vector3d, double, rotational_velocity_verlet_half,
-        sinter_step_handler_double, contact_force_functor<Eigen::Vector3d, double>,
-        sinter_functor<Eigen::Vector3d, double>> system(x0,
+        alt_sinter_step_handler_double, alt_sinter_functor<Eigen::Vector3d, double>> system(x0,
             v0, theta0, omega0, 0.0, Eigen::Vector3d::Zero(), 0.0, step_handler_instance,
-            contact_force_model, sinter_model);
+            sinter_model);
+
+    Eigen::Vector3d center_0 = center_of_mass(system.get_x());
 
     auto start_time = std::chrono::high_resolution_clock::now();
     for (size_t n = 0; n < n_steps; n ++) {
-        if (n % dump_period == 0) {
-            std::cout << "Dump #" << n / dump_period << std::endl;
-            write_particles("run", system.get_x(), system.get_theta(), r_part);
-            write_spring_connectors("run", r_part, sinter_model.spring_connectors,
-                                    contact_force_model.enabled_contacts);
-            write_necks("run", system.get_x(), r_part, contact_force_model.enabled_contacts);
-        }
         system.do_step(dt);
     }
     auto end_time = std::chrono::high_resolution_clock::now();
     auto duration = std::chrono::duration_cast<std::chrono::seconds>(end_time - start_time).count();
     std::cout << "Elapsed time: " << duration << " sec" << std::endl;
+
+    Eigen::Vector3d center_f = center_of_mass(system.get_x());
+
+    double rg = radius_of_gyration(system.get_x());
+    double center_dist = (center_f - center_0).norm();
+
+    double target_rg = 3.95981e-08;
+    double target_center_dist = 2.35702e-07;
+
+    double rg_tolerance = 1.0; // Percent
+    double center_dist_tolerance = 1.0; // Percent
+
+    if (abs(center_dist - target_center_dist) / target_center_dist > center_dist_tolerance / 100.0 ||
+        abs(rg - target_rg) / target_rg > rg_tolerance / 100.0)
+        return EXIT_FAILURE;
+
+    return 0;
 }
